384114237d359ce874f299ece057a243315f8598
Gaby Colver <gabriellacolver@gmail.com>
2022-04-02 19:50:59 -0600

    changing port number

--- back-end/server.js
+++ back-end/server.js
@@ -135,4 +135,4 @@ app.delete('/api/playlists/:id', async (req, res) => {
 });
 
 
-app.listen(3000, () => console.log('Server listening on port 3000!'));
+app.listen(4000, () => console.log('Server listening on port 4000!'));
